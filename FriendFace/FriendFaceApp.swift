3d05f831873232d0762b0e69f6e24a6e0a812d6c
JooMin Choi <joomin.choi@clearscore.com>
2022-10-18 14:01:43 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by JooMin Choi on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
